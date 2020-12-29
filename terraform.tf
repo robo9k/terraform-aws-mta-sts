9e14ca614910515e995e1912a8c48a5811c1e2a3
robot9001 <robo9k@symlink.io>
2020-12-29 23:42:46 +0100

    Replace `variable_validation` experiment with version requirement for Terraform and AWS provider

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,11 @@
 terraform {
-  experiments = [variable_validation]
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    aws = {
+      # TODO: this is not neccessarily the minimum required version
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
