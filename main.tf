c46e151183d74eeb702da976713654da12ce0ab9
robot9001 <robo9k@symlink.io>
2020-12-29 23:43:54 +0100

    Use `for_each` for ACM DNS validation

--- main.tf
+++ main.tf
@@ -59,16 +59,25 @@ resource "aws_acm_certificate" "default" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  zone_id = data.aws_route53_zone.default.zone_id
-  name    = aws_acm_certificate.default.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.default.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.default.domain_validation_options[0].resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.default.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.default.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 # Cloudfront distribution
